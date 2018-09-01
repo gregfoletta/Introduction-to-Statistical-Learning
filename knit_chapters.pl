4a91e0855dfd073bcdcc90017e02c212e705ecce
Greg Foletta <greg@foletta.org>
2018-09-01 12:19:48 +1000

    knit_chapters script now takes into account .Rmd versus .md mtime If the .md file was modified after the corresponding .Rmd file, then we don't bother to knit the file. This really helps when it comes to files that may run a simulation and take a long time to generate

--- knit_chapters.pl
+++ knit_chapters.pl
@@ -9,6 +9,7 @@ use Pod::Usage;
 use Carp;
 use File::Find;
 use File::Spec::Functions;
+use File::stat;
 use Statistics::R;
 use Cwd qw(getcwd); 
 
@@ -48,18 +49,19 @@ GetOptions(
 
 
 # Find the .Rmd files
-my @rmd_files;
-find( sub { push @rmd_files, [$File::Find::dir, $_] if m{\.Rmd$} }, '.' );
+my @rmd_files = files_to_knit();
 
-use constant {
-    DIR => 0,
-    FILE => 1,
-};
 
 my $base_dir = getcwd();
 
 for my $file_r (@rmd_files) {
-    # Change to the directory
+    use constant {
+        DIR => 0,
+        FILE => 1,
+    };
+
+
+    # Change to the directory the file is in.
     chdir $file_r->[DIR];
 
     my $R = Statistics::R->new;
@@ -81,7 +83,40 @@ for my $file_r (@rmd_files) {
 }
 
 
+# This sub searches for .Rmd files from the current working directory down.
+# If there is a corresponding .md file that was modifed after the .Rmd, the .Rmd
+# is not added. Otherwise it is added to the list of files to be knitted.
+sub files_to_knit {
+    my %mtime_of_md_files;
+    my @files_to_knit;
 
+    find( sub { 
+            my $f = $File::Find::name; 
+            my @matches = $f =~ m{(.*)\.(R?md)$};
+            return unless @matches == 2;
+            my ($path_and_filename, $suffix) = @matches;
+
+            $mtime_of_md_files{ $path_and_filename }{ $suffix }{mtime} = stat($_)->mtime();
+            $mtime_of_md_files{ $path_and_filename }{ $suffix }{dir} = $File::Find::dir; 
+        },
+        '.'
+    );
+
+
+    for my $file_without_suffix (keys %mtime_of_md_files) {
+        # Continute on if there isn't a .Rmd file
+        next unless exists $mtime_of_md_files{$file_without_suffix}{Rmd};
+
+        # If the '.md' mtime is after the '.Rmd' mtime, we skip;
+        next if $mtime_of_md_files{$file_without_suffix}{md}{mtime} >= $mtime_of_md_files{$file_without_suffix}{Rmd}{mtime};
+
+        # Push the directory and the filename into an ARRAYREF
+        push @files_to_knit, [$mtime_of_md_files{$file_without_suffix}{Rmd}{dir}, "$file_without_suffix.Rmd"];
+    }
+
+    return @files_to_knit;
+
+}
 
 
 
